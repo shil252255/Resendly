f07a958e11e8f8c6c80277e1bef71eca9dbc73bd
schil <sh.il252255@gmail.com>
2021-04-19 13:40:36 +0300

    INIT

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import json
+from telethon import TelegramClient, events
 from settings import API_ID, API_HASH, SESSION_NAME, FROM_CHAT_NAME, TO_CHAT_NAME
+
+client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
 
 
 if __name__ == '__main__':
