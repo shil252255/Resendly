5d62daf86c565cfa1ef24fc9d3eed16ec5d402e2
schil <sh.il252255@gmail.com>
2021-04-19 13:34:37 +0300

    INIT

---
+++ main.py
@@ -0,0 +1,5 @@
+from settings import API_ID, API_HASH, SESSION_NAME, FROM_CHAT_NAME, TO_CHAT_NAME
+
+
+if __name__ == '__main__':
+    pass
---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+API_ID = os.environ["TG_API_ID_SERG"]
+API_HASH = os.environ["TG_API_HASH_SERG"]
+SESSION_NAME = os.environ["SESSION_NAME_SERG"]
+FROM_CHAT_NAME = os.environ["FROM_CHAT_NAME"]
+TO_CHAT_NAME = os.environ["TO_CHAT_NAME"]
+
